89aef1e50b5e06e52d12250bae2b12b069203d80
Eris Silva <124813736+ErisSilvar@users.noreply.github.com>
2023-04-15 02:08:31 +0000

    Calculando a área do Retangulo (b * h);

---
+++ Retangulo.cs
@@ -0,0 +1,33 @@
+/*using System;
+
+public class Retangulo
+{
+    private int Base;
+    private int altura;
+
+    public void SetBase(int Base)
+    {
+        this.Base = Base;
+    }
+
+    public int GetBase()
+    {
+        return Base;
+    }
+
+    public void SetAltura(int altura)
+    {
+        this.altura = altura;
+    }
+
+    public int GetAltura()
+    {
+        return altura;
+    }
+
+    public int CalcularArea()
+    {
+        return Base * altura;
+    }
+
+}*/
--- main.cs
+++ main.cs
@@ -1,46 +1,59 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
+class Program
+{
+    public static void Main(string[] args)
+    {
 
-    
-    //calculo da área do quadrado.
-    
-    /*Quadrado q1;
-    q1 = new Quadrado();
-    q1.SetLado(3);
 
-    Console.WriteLine("Lado: " + q1.GetLado());
-    Console.WriteLine("Area : " + q1.CalcularArea());
-    Quadrado q2;
-    q2 = new Quadrado();
-    q2.SetLado(5);
+        //calculo da área do quadrado.
 
-    Console.WriteLine("Lado: " + q2.GetLado());
-    Console.WriteLine("Area: " + q2.CalcularArea());*/
+        /*Quadrado q1;
+        q1 = new Quadrado();
+        q1.SetLado(3);
 
-    //calculo da área do circulo.
-    
-    /*Circulo c1;
-    c1 = new Circulo();
-    c1.SetRaio(3);
+        Console.WriteLine("Lado: " + q1.GetLado());
+        Console.WriteLine("Area : " + q1.CalcularArea());
+        Quadrado q2;
+        q2 = new Quadrado();
+        q2.SetLado(5);
 
-    Console.WriteLine("Raio: " + c1.GetRaio());
-    Console.WriteLine("Area: " + c1.CalcularArea());*/
+        Console.WriteLine("Lado: " + q2.GetLado());
+        Console.WriteLine("Area: " + q2.CalcularArea());*/
 
-    //Cilindro
-    /*
+        //calculo da área do circulo.
 
-    Console.WriteLine ("Digite o raio do cilindro: ");
-    UInt32 raio = UInt32.Parse(Console.ReadLine());
+        /*Circulo c1;
+        c1 = new Circulo();
+        c1.SetRaio(3);
 
-    Console.WriteLine ("Digite a altura do cilindro: ");
-    UInt32 altura = UInt32.Parse(Console.ReadLine());
+        Console.WriteLine("Raio: " + c1.GetRaio());
+        Console.WriteLine("Area: " + c1.CalcularArea());*/
 
-    Cilindro cilindro = new Cilindro(raio, altura);
-    double area = cilindro.CalcularArea();
+        //calculo da área do Cilindro
+        /*
 
-    Console.WriteLine("A área do cilindro é: " + area);
-    Console.ReadLine();*/
-  }
+        Console.WriteLine ("Digite o raio do cilindro: ");
+        UInt32 raio = UInt32.Parse(Console.ReadLine());
+
+        Console.WriteLine ("Digite a altura do cilindro: ");
+        UInt32 altura = UInt32.Parse(Console.ReadLine());
+
+        Cilindro cilindro = new Cilindro(raio, altura);
+        double area = cilindro.CalcularArea();
+
+        Console.WriteLine("A área do cilindro é: " + area);
+        Console.ReadLine();*/
+
+        //calculo da área do Retangulo
+
+        /*Retangulo r1;
+        r1 = new Retangulo();
+        r1.SetBase(10);
+        r1.SetAltura(2);
+
+        Console.WriteLine("Base: " + r1.GetBase());
+        Console.WriteLine("Altura: " + r1.GetAltura());
+        Console.WriteLine("Area: " + r1.CalcularArea());*/
+    }
 }
